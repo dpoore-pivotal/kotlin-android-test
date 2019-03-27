1544773956d53a7e95587dff31049c66bbdf5f5a
David Poore <dpoore@pivotal.io>
2019-03-27 13:59:24 -0600

    add item works

--- app/src/main/java/com/example/recyclerviewtest/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewtest/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.recyclerviewtest
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.view.View
+import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,12 +15,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // initial test items, delete after we can add our own
-        listItems.add("foo")
-        listItems.add("bar")
-        listItems.add("baz")
-
         rv_list.layoutManager = LinearLayoutManager(this)
         rv_list.adapter = ListItemAdapter(listItems, this)
+    }
+
+    fun addItem(view: View) {
+        val newItemTextInput = findViewById<EditText>(R.id.add_item_input)
+        val newItemText = newItemTextInput.text.toString()
+        listItems.add(newItemText)
+        newItemTextInput.text.clear()
+        rv_list.adapter?.notifyDataSetChanged()
     }
 }
